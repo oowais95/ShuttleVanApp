25d5ed4d3895460a2c6d93674c7e421a29ea9754
STUDENT <smowais.firdousi@gmail.com>
2023-11-04 19:32:44 +0500

    initial project structure

---
+++ src/test/java/com/shuttleVan/TransportApp/TransportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuttleVan.TransportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
